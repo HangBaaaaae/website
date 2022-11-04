488f45a2a47ba85b13cb045234dff0d08eec30a4
HangBaaaaae <hyung_bin@kakao.com>
2022-11-04 10:17:45 +0900

    11-04

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,23 @@
 var day_date = new Date() ; // 오늘 날짜
-var work_start = "2015-07-01"; // 나의 경력 시작일자
-var working_date = day_date.getTime() - work_start.getTime() ;
-var working_day = working_date / (1000*60*60*24) ;
-console.log(working_day);
+var work_start = new Date (2015, 05, 01); // 나의 경력 시작일자
+
+
+var now_year = day_date.getFullYear();
+var now_month = ('0' + (day_date.getMonth() + 1)).slice(-2);
+var now_day = ('0' + day_date.getDate()).slice(-2);
+
+var work_year = work_start.getFullYear();
+var work_month = ('0' + (work_start.getMonth())).slice(-2);
+var work_day = ('0' + work_start.getDate()).slice(-2);
+
+
+
+var datestring = now_year + '-' + now_month + '-' + now_day ;
+var workstring = work_year + '-' + work_month + '-' + work_day ;
+var total_work = new Date(datestring)- (workstring) / (1000*60*60*24);
+
+
+console.log(datestring);
+console.log(workstring);
+console.log(total_work);
+
